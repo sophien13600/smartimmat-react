3a35b594cd418f963505ca49e05047e10a842fa9
Sophien <bouderbala.s@gmail.com>
2025-09-29 11:36:20 +0200

    feat: Navbar ajout navbar front

---
+++ smart immat/src/router/Routes.jsx
@@ -0,0 +1,12 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "../views/Home";
+const AppRoutes = () => {
+
+   return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
+}
+
+export default AppRoutes
